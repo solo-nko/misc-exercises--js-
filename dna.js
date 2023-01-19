32b55621aa9418fda4b5a82735ff1acbe38bea06
nko <solowingrazgriz@outlook.com>
2023-01-19 15:08:40 +0900

    Expand pAequorFactory with compareDNA() method, getters & setters

--- dna.js
+++ dna.js
@@ -18,10 +18,10 @@ const returnRandBase = () => {
   function pAequorFactory(numVal, dnaVal = mockUpStrand())
   {
     return {
-        specimenNum: numVal,
-        dna: dnaVal,
+        _specimenNum: numVal,
+        _dna: dnaVal,
         mutate(){
-            let sample = Array.from(this.dna);
+            let sample = Array.from(this._dna);
             let newBase = sample[0];
             while(newBase === sample[0]) {
                 newBase = returnRandBase();
@@ -30,7 +30,30 @@ const returnRandBase = () => {
             this.dna = sample;
         },
         compareDNA(otherSpecimen){
-            
-        }
+			let otherStrand = otherSpecimen.dna;
+			let numberMatching = 0;
+			for (i = 0; i < this._dna.length; i++)
+			{
+				if (otherStrand[i] === this._dna[i])
+				{
+					numberMatching++;
+				}
+			}
+			let percentage = (numberMatching/this._dna.length);
+			percentage = percentage.toFixed(2)*100;
+			if (percentage != 0)
+			{
+				console.log(`Specimen #${numVal} and Specimen #${otherSpecimen.specimenNum} share ${percentage}% of their DNA strands.`);
+			} else
+			{
+				console.log(`Specimen #${numVal} and Specimen #${otherSpecimen.specimenNum} do not share any DNA.`);
+			}
+        },
+		get dna(){
+			return this._dna;
+		},
+		get specimenNum(){
+			return this._specimenNum;
+		}
     };
   };

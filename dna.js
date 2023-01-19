6f3079d15c604b9d63dde1ca3077a8d315c847bb
nko <solowingrazgriz@outlook.com>
2023-01-19 14:04:44 +0900

    Update pAequorFactory() to modify dna property properly

--- dna.js
+++ dna.js
@@ -4,7 +4,7 @@ const returnRandBase = () => {
     return dnaBases[Math.floor(Math.random() * 4)];
   };
   
-  // Returns a random single stand of DNA containing 15 bases
+  // Returns a random single strand of DNA containing 15 bases
   const mockUpStrand = () => {
     const newStrand = [];
     for (let i = 0; i < 15; i++) {
@@ -15,17 +15,22 @@ const returnRandBase = () => {
 
   console.log(mockUpStrand());
 
-  function pAequorFactory(numVal, dnaVal)
+  function pAequorFactory(numVal, dnaVal = mockUpStrand())
   {
     return {
         specimenNum: numVal,
         dna: dnaVal,
         mutate(){
-            let sample = this.dna;
-            while(sample === this.dna) {
-                sample = returnRandBase();
+            let sample = Array.from(this.dna);
+            let newBase = sample[0];
+            while(newBase === sample[0]) {
+                newBase = returnRandBase();
             }
+            sample[0] = newBase;
             this.dna = sample;
+        },
+        compareDNA(otherSpecimen){
+            
         }
     };
   };
